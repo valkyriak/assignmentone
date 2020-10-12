11cfd9c41d04b997da2c66d4093be3572f34d6e2
Kane Clerke <kane.clerke@gmail.com>
2020-10-12 21:05:35 +1000

    yeet

--- server/routes/get-channels.js
+++ server/routes/get-channels.js
@@ -1,8 +1,8 @@
 module.exports = function(channelCollect, app, ObjectID) {
 
     const fs = require('fs');
-    app.get('/api/get-channels', function(req,res) {
-        let objectid = ObjectID(req.params.room_id);
+    app.get('/api/get-channels/:group_id', function(req,res) {
+        let objectid = ObjectID(req.params.group_id);
         channelCollect.find({room_id: objectid}).toArray( (err, result) => {
             if(err) { 
                 return res.sendStatus(400); 
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { SocketService } from '../services/socket.service';
+
 import { Router } from '@angular/router';
-import { User } from '../user';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
+
+import { SocketService } from '../services/socket.service';
+import { UsersService } from '../services/users.service';
+
 
 const BACKEND_URL = 'http://localhost:3000';
 const httpOptions = {
@@ -16,23 +19,24 @@ const httpOptions = {
 })
 export class MainComponent implements OnInit {
 
-  //User information declarations for parsing data
- 
-  public groups = [];
-  public group_select: any = null;
-
-  public channels: any = null;
-  public channel_select: any = null;
-
   //Chat element declarations for parsing data
   newMessage:String;
   messageArray:Array<{user:String, message:String}> = [];
   ioConnection:any;
 
-  constructor(private socketService: SocketService, private router: Router, private httpClient: HttpClient) { 
+  constructor(private socketService: SocketService, private router: Router, private httpClient: HttpClient, private userService: UsersService) { }
+    
+    //User information declarations for parsing data
+    private currentUser: boolean;
+    private currentUserDetails: any;
+    private currentUserIsGA: boolean = false;
+   
+    public groups = [];
+    public group_select: any = null;
   
-
-  }
+    public channels: any = null;
+    public channel_select: any = null;
+  
 
     // Joins the current user to their selected room and channel
     join(){
@@ -45,7 +49,9 @@ export class MainComponent implements OnInit {
     //Sends a message to the currently joined chatroom.
     sendMessage(){
       console.log(this.newMessage);
-        this.socketService.sendMessage({user:this.currentUser.username, room:this.room, message:this.newMessage});
+      let data = {username: this.currentUserDetails.username, room: this.channel_select.id, message: this.newMessage};
+      this.socketService.sendMessage(data);
+      this.newMessage = '';
     }
 
 
@@ -54,47 +60,65 @@ export class MainComponent implements OnInit {
 // On load grab the current user details, grabs groups from local storage
   ngOnInit(): void {
 
-    this.getGroups()
-    this.initChatConnection()
-    
-      try {
-        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        if(this.currentUser){
-          this.email = this.currentUser.email;
-          this.username = this.currentUser.username;
-          this.role = this.currentUser.role;
-          this.id = this.currentUser.id;
-        }
-      }
-      catch(err){
-        alert ("Not logged in");
-        this.router.navigateByUrl('/login');
-      }
+    if(this.userService.current_user) {
+      this.currentUser = this.userService.current_user;
+    } else {
+      alert ("Please sign in")
+      this.router.navigateByUrl('/login');
+    }
+
+    if (this.currentUser && this.userService.userrole.includes(this.userService.RetrieveUserDetails)) {
+      this.currentUserDetails = this.userService.RetrieveUserDetails;
+    } else {
+      this.router.navigateByUrl('/login');
   }
 
-  //Fetches the groups from JSON file
-  public getGroups() {
+    this.getGroups()
+    this.initChatConnection()
 
-    this.httpClient.post(BACKEND_URL + '/api/groups', httpOptions)
-    .subscribe((data: any) => {
-        if (data) {
-            data.forEach(g => {
-                g.users.forEach(user => {
-                    if (user == this.username) {
-                        this.groups.push(g);
-                    }
-                });
-            });
+
+  }
+
+  //Fetches the groups from the server side which then retrieves from mongo
+  // Also checks if the current user is a group assistant
+  private getGroups() {
+    this.groups = [];
+    this.httpClient.get(BACKEND_URL + '/api/get-groups', httpOptions).subscribe((data: any) => {
+        if (this.currentUserDetails.role == 'super-admin' || this.currentUserDetails.role == 'group-admin') {
+            this.groups = data;
         } else {
-            alert('No groups, please add some in the admin panel');
+            data.forEach(group => {
+                console.log(group.group_users);
+                const activeGA = Boolean(group.group_assistants.find(x => x === this.currentUserDetails.id));
+                const activeuser = Boolean(group.group_users.find(x => x === this.currentUserDetails.id));
+                if (activeGA || activeuser) {
+                    activeGA ? this.currentUserIsGA = true : this.currentUserIsGA = false;
+                    this.groups.push(group);
+                } else {
+                    console.log('not in any groups');
+                }
+            });
         }
-
     });
 }
+
+//Checks within the groups to find channels, checks current user roles and then displays according data
 public getChannels(group) {
   this.channels = [];
-  this.httpClient.get(BACKEND_URL + '/api/get-channels'/ + group._id, httpOptions).subscribe((data: any) => {
-
+  this.httpClient.get(BACKEND_URL + '/api/get-channels/' + group._id, httpOptions).subscribe((data: any) => {
+    if (this.currentUserDetails.role == 'super-admin' || this.currentUserDetails.role == 'group-admin' || this.currentUserIsGA ) {
+      this.channels = data;
+    } else {
+      data.forEach(channel => {
+        console.log(channel);
+        const active = Boolean(channel.channel_users.find(i => i == this.currentUserDetails.id));
+        if (active) {
+          this.channels.push(channel)
+        } else {
+          console.log("not in any channels currently");
+        }
+      });
+    }
   });
 }
 
